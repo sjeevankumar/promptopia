326da2dcbf34778bac6cb1c34280b8fad2c910d8
sjeevankumar <sjeevankumar89@gmail.com>
2024-06-19 08:35:30 +0530

    feat:initial setup

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+
+const Home = () => {
+  return (
+    <div>Welcome Home</div>
+  )
+}
+
+export default Home
--- app/posts/new/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>New</div>
-  )
-}
-
-export default page
